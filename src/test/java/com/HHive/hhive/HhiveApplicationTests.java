7af3fc95b69c9c979fafee3ead215ad920bcdd69
김은채 <ke808762@gmail.com>
2024-01-11 16:21:48 +0900

    SpringBootTest 주석처리, Task -> test

--- src/test/java/com/HHive/hhive/HhiveApplicationTests.java
+++ src/test/java/com/HHive/hhive/HhiveApplicationTests.java
@@ -3,7 +3,7 @@ package com.HHive.hhive;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class HhiveApplicationTests {
 
     @Test
